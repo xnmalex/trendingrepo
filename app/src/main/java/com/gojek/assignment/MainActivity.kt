ccd3c3e1b1d17c7f5be2fb95e63b5582b0a9010c
Alex Gui <alex.gui@orion.co.com>
2020-01-12 16:04:49 +0800

    initialise project with target API 19 (kitkat)

---
+++ app/src/main/java/com/gojek/assignment/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.gojek.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gojek.assignment.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/gojek/assignment/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.gojek.assignment.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
