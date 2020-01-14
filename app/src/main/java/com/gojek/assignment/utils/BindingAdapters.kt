9c10867d48bb8dbc41f3ea2a9e57512324c2dfad
Alex Gui <alex.gui@orion.co.com>
2020-01-14 23:42:12 +0800

    added facebook shimmer for loading effect

--- app/src/main/java/com/gojek/assignment/utils/BindingAdapters.kt
+++ app/src/main/java/com/gojek/assignment/utils/BindingAdapters.kt
@@ -1,8 +1,15 @@
 package com.gojek.assignment.utils
 
+import android.graphics.Color
+import android.graphics.drawable.LayerDrawable
+import android.text.TextUtils
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.ShimmerFrameLayout
+import com.gojek.assignment.R
 import com.squareup.picasso.Picasso
 
 
@@ -14,4 +21,19 @@ fun setAdapter(view: RecyclerView, adapter: RecyclerView.Adapter<*>) {
 @BindingAdapter("imageUrl")
 fun setImageUrl(view: ImageView, imagePath: String) {
     Picasso.get().load(imagePath).into(view)
+}
+
+@BindingAdapter("stopShimmer")
+fun setStopShimmer(view: ShimmerFrameLayout, isLoading:Boolean) {
+    view.visibility = if (isLoading)  View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("isLanguageVisible")
+fun setLanguageVisible(view: LinearLayout, language:String?) {
+    view.visibility = if (TextUtils.isEmpty(language))  View.GONE else View.VISIBLE
+}
+
+@BindingAdapter("languageColor")
+fun setLanguageColor(view: View, languageColor:String?) {
+    if (!TextUtils.isEmpty(languageColor))   view.setBackgroundColor(Color.parseColor(languageColor))
 }
